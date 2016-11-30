b8b29ff744e808a524bcf7189bddea0761f94df9
Justin Kim <justinkim@mishipay.com>
2016-11-30 13:11:02 +0000

    initial commit

---
+++ did_database/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DidDatabaseConfig(AppConfig):
+    name = 'did_database'
---
+++ did_webapp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DidWebappConfig(AppConfig):
+    name = 'did_webapp'
